257b807d8bc9199a8df71da32b36fe1d623d1a72
RobertMisch <robertmisch94@gmail.com>
2020-05-29 17:11:39 -0400

    Robert-Misch

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -3,7 +3,7 @@
 // const knexConfig = require('../knexfile.js');
 
 // module.exports = knex(knexConfig.production);
-const dbEngine = processenv.DB || 'development';
+const dbEngine = process.env.DB || 'development';
 const config = require('../knexfile')[dbEngine];
 
 module.exports = require('knex')(config);
